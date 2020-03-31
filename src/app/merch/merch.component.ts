ed92b0701a1181e7ce77a28038f303f0eb1f718a
RossEpsteinKY <ross.epstein@hotmail.com>
2020-03-31 09:49:31 -0400

    initial merch layout

--- src/app/merch/merch.component.ts
+++ src/app/merch/merch.component.ts
@@ -9,6 +9,12 @@ export class MerchComponent implements OnInit {
 
   constructor() { }
 
+  public albums = [
+    {name: 'Songs From The Briarwood', img: '../../assets/images/albumart/briarwood.png', itunes: 'https://music.apple.com/us/album/songs-from-the-briarwood/1502365122?uo=4&app=itunes&at=1001lry3&ct=dashboard'},
+    {name: 'Songs From The Briarwood', img: '../../assets/images/albumart/briarwood.png', itunes: 'https://music.apple.com/us/album/songs-from-the-briarwood/1502365122?uo=4&app=itunes&at=1001lry3&ct=dashboard'},
+    {name: 'Songs From The Briarwood', img: '../../assets/images/albumart/briarwood.png', itunes: 'https://music.apple.com/us/album/songs-from-the-briarwood/1502365122?uo=4&app=itunes&at=1001lry3&ct=dashboard'},
+  ]
+
   ngOnInit() {
   }
 
