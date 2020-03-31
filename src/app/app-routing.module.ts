cb289ca55684b439a68f3da20bc7140a3d22f22d
RossEpsteinKY <ross.epstein@hotmail.com>
2020-03-31 09:31:30 -0400

    Generated merch component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {MerchComponent} from "./merch/merch.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
+  { path: 'merch', component: MerchComponent},
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { MerchComponent } from './merch/merch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    MerchComponent
   ],
   imports: [
     BrowserModule,
