528051f6ce81b23faa46c6b761e03faf8a4335a7
RossEpsteinKY <ross.epstein@hotmail.com>
2020-03-31 10:14:25 -0400

    installing font awesome, added images

--- src/app/merch/merch.component.ts
+++ src/app/merch/merch.component.ts
@@ -10,9 +10,20 @@ export class MerchComponent implements OnInit {
   constructor() { }
 
   public albums = [
-    {name: 'Songs From The Briarwood', img: '../../assets/images/albumart/briarwood.png', itunes: 'https://music.apple.com/us/album/songs-from-the-briarwood/1502365122?uo=4&app=itunes&at=1001lry3&ct=dashboard'},
-    {name: 'Songs From The Briarwood', img: '../../assets/images/albumart/briarwood.png', itunes: 'https://music.apple.com/us/album/songs-from-the-briarwood/1502365122?uo=4&app=itunes&at=1001lry3&ct=dashboard'},
-    {name: 'Songs From The Briarwood', img: '../../assets/images/albumart/briarwood.png', itunes: 'https://music.apple.com/us/album/songs-from-the-briarwood/1502365122?uo=4&app=itunes&at=1001lry3&ct=dashboard'},
+    {name: 'Songs From The Briarwood',
+      img: '../../assets/images/albumart/briarwood.png',
+      itunes: 'https://music.apple.com/us/album/songs-from-the-briarwood/1502365122?uo=4&app=itunes&at=1001lry3&ct=dashboard'
+    },
+
+    {name: 'From Under The Northern Lights',
+      img: '../../assets/images/albumart/northernlights.png',
+      itunes: 'https://music.apple.com/us/album/songs-from-the-briarwood/1502365122?uo=4&app=itunes&at=1001lry3&ct=dashboard'
+    },
+
+    {name: "We're All Going To Helheim (EXPLICIT)",
+      img: '../../assets/images/albumart/helheim.png',
+      itunes: 'https://music.apple.com/us/album/songs-from-the-briarwood/1502365122?uo=4&app=itunes&at=1001lry3&ct=dashboard'
+    },
   ]
 
   ngOnInit() {
