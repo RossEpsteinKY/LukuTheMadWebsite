b377a10dc3cbe3f1f96a3c8b448ecf3d1e009417
RossEpsteinKY <ross.epstein@hotmail.com>
2020-04-02 12:49:57 -0400

    photos added

--- src/app/presskit/presskit.component.ts
+++ src/app/presskit/presskit.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PresskitComponent implements OnInit {
 
+  public promoPhotos = [
+    {url: '../assets/images/promo/luku_promo_1.jpg'},
+    {url: '../assets/images/promo/luku_promo_2.jpg'},
+    {url: '../assets/images/promo/luku_promo_3.jpg'},
+    {url: '../assets/images/promo/luku_promo_4.jpg'},
+    {url: '../assets/images/promo/luku_promo_5.jpg'},
+    {url: '../assets/images/promo/luku_promo_6.jpg'},
+    {url: '../assets/images/promo/luku_promo_7.jpg'},
+    {url: '../assets/images/promo/luku_promo_8.jpg'},
+    {url: '../assets/images/promo/luku_promo_9.jpg'},
+    {url: '../assets/images/promo/luku_promo_10.jpg'},
+    {url: '../assets/images/promo/luku_promo_logo.png'},
+    {url: '../assets/images/promo/luku_promo_logo_small.png'},
+  ]
+
   constructor() { }
 
   ngOnInit() {
