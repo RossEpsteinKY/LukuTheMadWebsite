5304ac5648d83fdb9e4c35a3eca08c9e0332b2a4
Ross Epstein <rossepsteinforge@gmail.com>
2020-06-28 11:51:54 -0400

    added kids songs and stories

--- src/app/songlist/songlist.component.ts
+++ src/app/songlist/songlist.component.ts
@@ -13,30 +13,39 @@ export class SonglistComponent implements OnInit {
   public cleanSongs = [
     "Black Velvet Band",
     "Drunken Lullabies",
-        "Chandler’s Wife",
-  "Twilight of the Thunder God",
-  "Lifeblood",
-  "A Song For Odin",
-  "Mari Mac",
-  "Whiskey In The Jar",
-  "I’s The By",
-  "Isn’t It Grand Boys?",
-  "Captain Kidd",
-  "Run Run Away",
-  "Nancy Whiskey",
-  "Tell Me Ma",
-  "Yggdrasil",
-  "Fields of Gold",
-  "If I Should Fall From Grace With God",
-  "Wild Mountain Thyme",
-  "Mingulay Boat Song",
-  "Galway Girl",
-  "The Truth About The Vikings (original)"];
+    "Twilight of the Thunder God",
+    "Lifeblood (A Viking Drinking Song)",
+    "A Song For Odin",
+    "Mari Mac",
+    "Whiskey In The Jar",
+    "I’s The By",
+    "Isn’t It Grand Boys?",
+    "Captain Kidd",
+    "Run Run Away",
+    "Nancy Whiskey",
+    "Tell Me Ma",
+    "Yggdrasil",
+    "Fields of Gold",
+    "If I Should Fall From Grace With God",
+    "Wild Mountain Thyme",
+    "Mingulay Boat Song",
+    "Galway Girl",
+    "The Truth About The Vikings (original)",
+    "Places Unseen (original)",
+    "Rais Your Horns To The Sky (original)",
+    "Of Silk And Fine Furs",
+    "Loch Lomond",
+    "Paddy West",
+    "Under The Glow (original)",
+
+
+  ];
 
   public bawdySongs = [
-    "Oreiley’s Daughter",
+    "O'reiley’s Daughter",
     "First Of May",
     "XY(ZED)",
+    "Chandler’s Wife",
     "Big Bosomed Girls",
     "Betty West (original)",
     "Mingulay Corpse Song (original)",
@@ -45,8 +54,22 @@ export class SonglistComponent implements OnInit {
     "The Widow and the Devil",
     "Charlotte The Harlot",
     "Johnny Be Fair",
+    "Grandfather's Song",
+    "Boned Her In The Alley (original)",
+    "To Hell With The Company (original)",
+];
+
+  public stories = [
+    "The Tale Of Ymir",
+    "Tale of Yggdrasil",
+    "The Marriage of Njord and Skadi",
+    "The Creation of Sleipnir"
+  ];
 
 
+  public kidsSongs = [
+    "You're Welcome",
+    "Part Of Your World",
 
 ];
 
@@ -60,6 +83,18 @@ export class SonglistComponent implements OnInit {
     });
 
     this.bawdySongs.sort(function(a, b){
+      if (a < b) { return -1; }
+      if (a > b) { return 1; }
+      return 0;
+    });
+
+    this.kidsSongs.sort(function(a, b){
+      if (a < b) { return -1; }
+      if (a > b) { return 1; }
+      return 0;
+    });
+
+    this.stories.sort(function(a, b){
       if (a < b) { return -1; }
       if (a > b) { return 1; }
       return 0;
